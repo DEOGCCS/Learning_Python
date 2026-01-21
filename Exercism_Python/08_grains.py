761457849a9885e2cfeb0b70ac878321bd513e56
David Gomes <davidoliveiraccs@gmail.com>
2026-01-21 12:20:40 +0000

    Refactor total function for grain calculation
    Refactor total function to calculate total grains on all squares.

--- Exercism_Python/08_grains.py
+++ Exercism_Python/08_grains.py
@@ -4,10 +4,13 @@ def square(number):
     else:
         raise ValueError("square must be between 1 and 64")
 
-def total(square):
-    counter = 1
-    while square <= 64:
-        counter += 1
+def total():
+    result = 0
+    num = 1
+    while num <= 64:
+        result += 2**(num-1)
+        num += 1
+    return result
 
 num = int(input("add number"))
 print(square(num))
